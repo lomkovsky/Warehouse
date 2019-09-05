aa78d2744c8228f5b40c2f73a4a2e6b648ccb29d
lomkovsky <lomkovsky@gmail.com>
2019-09-05 16:21:42 +0300

    used short syntaxis

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-const database = require('./database.json')
-console.log(Object.keys(database.food).concat(Object.keys(database.electronic)).concat(Object.keys(database.stuff)));
+const database = require('./database.json');
 // set static view engine as pug
 app.set('view engine', 'pug');
 app.get('/', (req, res) => {
@@ -12,26 +11,26 @@ app.get('/', (req, res) => {
 app.get('/categories/:category_id', (req, res) => {
     res.render(`./categories/${req.params.category_id}`, {
         title: `${req.params.category_id}`,
-        database: database
+        database
     });
 });
 app.get('/categories', (req, res) => {
     res.render('./categories/categories', {
         title: 'categories',
-        database: database
+        database
     });
 });
 app.get('/products', (req, res) => {
     res.render('./products/products', {
         title: 'products',
-        database: database        
+        database        
     });
 });
 app.get('/products/:id', (req, res) => {
     res.render('./products/productsID', {
         title: 'productsID',
         id: req.params.id,
-        database: database 
+        database 
     });
 });
 // returns JSON

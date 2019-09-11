aeba4de40b850a68954f3aba2386ada7403d5c29
lomkovsky <lomkovsky@gmail.com>
2019-09-11 19:56:20 +0300

    refactored categorier router

--- src/routers/categories.js
+++ src/routers/categories.js
@@ -8,59 +8,55 @@ const Product = require('../models/product');
 router.get('/categories', async (req, res) => {
   try {
     const categories = await Category.find();
-    
-    res.render('./categories/categories', {
-      title: 'categories',
-      categories
-      });
+    res.send(categories);
   } catch (e) {
-    res.status(400).send(e);
+    res.status(500).send('Something went wrong');
   };
-  
 });
+
 // create a new categories
-router.post('/categories/', async (req, res) => {
+router.post('/categories', async (req, res) => {
   const category = new Category(req.body)
   try {
     await category.save();
     res.status(201).send(category);
   } catch (e) {
-    res.status(400).send(e);
+    res.status(400).send(e.message);
   };
 });
+
 // delete category by id
 router.delete('/categories/:id', async (req, res) => {
   try {
-  const category = await Category.findOne({ name : req.params.id });
-  const product = await Product.findOne({ category : category._id });
-  product.delete();
-  category.delete();
-  res.send(req.params.id + ' is deleted!')
+    const category = await Category.findOneAndDelete ({_id: req.params.id});
+    const product = await Product.find({ category: category._id });// many
+    if(!product.length === 0){
+      product.delete();
+    };
+    category.delete();
+    res.send()
   } catch (e) {
-    res.status(404).send(e);
+    res.status(404).send(e.message); 
   };
 });
+
 //update category
 router.patch('/categories/:id', async (req, res) => {
-  console.log(req.params.id)
-    try {
-      const category = await Category.findOne({ name: req.params.id });
-      console.log(category)
-      category.category = req.body.category;
-      console.log(category)
-      await category.save();
-      res.send(category);
-    } catch (e) {
-      res.status(404).send(e);
-    }
-  
-})
+  try {
+    const category = await Category.findByIdAndUpdate(req.params.id, {name: req.body.name}, {new: true});
+    await category.save();
+    res.send(category);
+  } catch (e) {
+    res.status(404).send(e.message);
+  };
+});
 
 router.get('/categories/:id', async (req, res) => {
-  const category = req.params.id;
-  res.render('./categories/categoryById', {
-    title: category,
-    category
-  });
+  try {
+  const category = await Category.findById(req.params.id);
+  res.send(category);
+} catch (e) {
+  res.status(404).send(e.message);
+};
 });
 module.exports = router;
--- src/routers/products.js
+++ src/routers/products.js
@@ -24,7 +24,7 @@ router.post('/products', async (req, res) => {
       console.log(newProduct);
       res.send(product);
     } catch (e) {
-      res.status(400).send(e);
+      res.status(400).send(e.message);
     }
   };
 });
@@ -33,42 +33,48 @@ router.post('/products', async (req, res) => {
 router.get('/products', async (req, res) => {
   try {
     const product = await Product.find();
-    console.log(product);
-    const newProduct = await Product
-        .find()
-        .populate('category');
-      console.log(newProduct);
+    // console.log(product);
+    const newProduct = await Product //
+        .find()  //
+        .populate('category');  //
+      // console.log(newProduct);
     res.render('./products/products', {
       title: 'products',
       product,
       newProduct
     });
   } catch (e) {
-    res.status(400).send(e);
+    res.status(400).send(e);//
   };
 });
 // delete product
 router.delete('/products/:id', async (req, res) => {
   try {
-    const product = await Product.findOne({ name: req.params.id });
-    product.delete();
+    const product = await Product.findOne({ name: req.params.id }); //
+    product.delete(); //
     res.send(req.params.id + ' is deleted!')
   } catch (e) {
     res.status(404).send(e);
   };
 });
 // update product
-router.patch('/products/:id', async (req, res) => {
+router.patch('/products/:name', async (req, res) => {
   const category = await Category.findOne({ name: req.body.category })
   if (!category) {
     res.status(404).send('Category not found!');
   } else {
     try {
-      const product = await Product.findOne({ ame: req.params.id });
-      const updates = Object.keys(req.body);
-      updates.forEach((update) => product[update] = req.body[update]);
-      await product.save();
-      res.send(product);
+      const product = await Product.findOne({ name: req.params.name });
+      console.log(req.body.name);
+      await product.update({name: req.body.name}, { new: true});      
+      console.log(product);
+      // const updates = Object.keys(req.body);//
+      // console.log(product);//
+      // updates.forEach((update) => product[update] = req.body[update]);//
+      const newProduct = await product.save();
+      // product = await product.save();
+      console.log(newProduct);
+      res.send(newProduct);
     } catch (e) {
       res.status(404).send(e);
     }
